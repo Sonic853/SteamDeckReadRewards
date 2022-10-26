59d9b36f67f7ac771bb007f9fc1b9a3ff38056b1
Sonic853 <sonic853@qq.com>
2022-10-26 11:30:28 +0800

    Steam style

---
+++ js/index.js
@@ -0,0 +1,20 @@
+/** @type {HTMLVideoElement} */
+const video = document.querySelector('#video');
+const overlay = document.querySelector('#overlay');
+const acceptButton = document.querySelector('#accept-button');
+function buttonClick1(event) {
+    event.preventDefault();
+    overlay.hidden = true;
+    video.play();
+    videoClick();
+}
+function videoClick(event) {
+    if (event) event.preventDefault();
+    const { documentElement } = document;
+    if (documentElement.requestFullscreen) documentElement.requestFullscreen();
+    else if (documentElement.mozRequestFullScreen) documentElement.mozRequestFullScreen();
+    else if (documentElement.webkitRequestFullscreen) documentElement.webkitRequestFullscreen();
+    else if (documentElement.msRequestFullscreen) documentElement.msRequestFullscreen();
+}
+acceptButton.addEventListener('click', buttonClick1);
+video.addEventListener('click', videoClick);
